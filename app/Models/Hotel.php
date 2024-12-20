a514342480f1ba31696d96656dcad723e13909ef
royyan <royanhusen007@gmail.com>
2024-12-20 16:05:02 +0700

    mengatur fillable & relationship

--- app/Models/Hotel.php
+++ app/Models/Hotel.php
@@ -2,9 +2,44 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Hotel extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'slug',
+        'thumbnail',
+        'link_gmaps',
+        'city_id',
+        'slug',
+        'country_id',
+        'address',
+        'start_level',
+    ];
+
+    public function city() {
+        return $this->belongsTo(City::class);
+    }
+
+    public function country() {
+        return $this->belongsTo(Country::class);
+    }
+
+    public function photos() {
+        return $this->hasMany(HotelPhoto::class);
+    }
+
+    public function rooms() {
+        return $this->hasMany(HotelRoom::class);
+    }
+
+    public function getLowestRoom() {
+        $minPrice = $this->rooms()->min('price');
+        return $minPrice ?? 0;
+    }
 }
--- app/Models/HotelPhoto.php
+++ app/Models/HotelPhoto.php
@@ -2,9 +2,20 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class HotelPhoto extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'photo',
+        'hotel_id',
+    ];
+
+    public function hotel() {
+        return $this->belongsTo(Hotel::class);
+    }
 }
