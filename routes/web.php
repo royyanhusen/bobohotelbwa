41b4d89e7d5f02ec76583a37654d7b4d9958be34
royyan <royanhusen007@gmail.com>
2024-12-21 10:23:39 +0700

    membuat route cms

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,10 @@
 <?php
 
+use App\Http\Controllers\CityController;
+use App\Http\Controllers\CountryController;
+use App\Http\Controllers\HotelBookingController;
+use App\Http\Controllers\HotelController;
+use App\Http\Controllers\HotelRoomController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -15,6 +20,29 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::prefix('admin')->name('admin.')->group(function() {
+        Route::middleware('can:manage cities')->group(function() {
+            Route::resource('cities', CityController::class);
+        });
+
+        Route::middleware('can:manage countries')->group(function() {
+            Route::resource('countries', CountryController::class);
+        });
+
+        Route::middleware('can:manage hotels')->group(function() {
+            Route::get('/add/room/{hotel:slug}', HotelRoomController::class, 'create')->name('hotel_room.create');
+            Route::post('/add/room/{hotel:slug}/store', HotelRoomController::class, 'store')->name('hotel_room.store');
+            Route::get('/hotel{hotel:slug}/room/{hotel_room}', HotelRoomController::class, 'edit')->name('hotel_room.edit');
+            Route::put('/room/{hotel_room}/update', HotelRoomController::class, 'update')->name('hotel_room.update');
+            Route::delete('/hotel/{hotel:slug}/delete/{hotel_room}', HotelRoomController::class, 'destroy')->name('hotel_room.destroy');
+        });
+
+        Route::middleware('can:manage hotel bookings')->group(function() {
+            Route::get('hotel_bookings', HotelBookingController::class);
+        });
+    });
+
 });
 
 require __DIR__.'/auth.php';
